a551f05d1eda615494c98252674655fd203666fd
Akhtyrskyi-Mykhailo <mikhaphantom@gmail.com>
2023-01-27 00:35:08 +0200

    Akhtyrskyi_lesson_12(.json, .csv)

--- Akhtyrskyi_lesson_12.py
+++ Akhtyrskyi_lesson_12.py
@@ -22,52 +22,86 @@ def cenzor_text(filename: str, words: str, save_file: str):
         rez_file.writelines(line)
 
 
-def count_words(filename: str) -> dict:
+def count_changed_words(filename: str, chenged_words: str) -> dict:
     """
-    На вход функция получает имя файла, функция возвращает словарь,
-    содержащий в качестве ключей слова из текстового файла, а в качестве значений их количество.
+    На вход функция получает имя файла и слова, которые были в нем заменены.
+    Функция возвращает словарь, содержащий в качестве ключей слова,которые были заменены,
+    а в качестве значений их количество.
     :param filename:
     :return: total -> dict
     """
     file = open(f'{filename}.txt', "r")
     text = file.read()
-    text = text.lower()
-    text_list = text.split()
-    reg_text_list = [word.strip('.,!?()') for word in text_list]
-    unique_words = set(reg_text_list)
-    total = {}
-    for w in unique_words:
-        total[w] = reg_text_list.count(w)
-    return total
+    text_list = text.lower().split()
+    edited_text_list = [word.strip('.,!?()') for word in text_list]
+    list_chenged_words = chenged_words.split()
+    count_words = {}
+    for word in list_chenged_words:
+        count_words[word] = edited_text_list.count(word)
+
+    return count_words
 
 
-def statistics(filename: str, changed_words: str, dict_word: dict, save_name: str):
+
+def file_information(file_name: str, dict_count: dict, save_filename: str) -> dict:
     """
     На вход функция получает:
-    -имя файла;
-    -слова, которые были заменены;
-    -словарь, содержащий в качестве ключей слова из текстового файла, а в качестве значений их количество;
-    -имя файла, в который был записан измененный текст.
-    В результате работы создается файл stat.json(формат данных JSON) со статисткой(обновляемый) в виде:
-    -название файла;
-    -список слов, которые были заменены;
-    -сколько раз каждое слово встретилось в тексте;
-    -имя файла, в который был записан измененный текст.
-    :param filename:
-    :param dict_word:
+    - имя файла, словарь с количеством измененных слов и имя файла, в который был записан измененный текст.
+    Функция возвращает словарь, где ключ - имя файла, значение - словарь с количеством измененных слов;
+    и имя файла, в который был записан измененный текст.
+    :param file_name:
+    :param dict_count:
+    :param save_filename:
+    :return: inf_file: -> dict
+    """
+    dict_file_information = {}
+    dict_file_information[f'{file_name}.txt'] = dict_count
+    dict_file_information['filename_with_changes'] = f'{save_filename}.txt'
+    return dict_file_information
+
+
+def save_in_json_file(information_dict: dict):
+    """
+    На вход функция получает словарь с информацией о фaйле.
+    В результате работы в файл stat.json, дописывается информация о новом цензурируемом файле.
+
+    :param information_dict: -> dict
     :return:
     """
     import json
     import os
-    file_infonation = {}
-    file_infonation['count_words'] = dict_word
-    file_infonation['changed_words'] = changed_words.split()
-    file_infonation['filename_with_changes'] = f'{save_name}.txt'
-    with open("stat.json", "a") as file:
-        if os.stat("stat.json").st_size == 0:
-            file.write("{}")
-    with open('stat.json', 'r') as read_file:
-        stat_dict = json.load(read_file)
-        stat_dict[f'{filename}.txt'] = file_infonation
+
+    if not os.path.isfile('stat.json'):            # проверяем существует ли файл stat.json
+        with open("stat.json", "w") as file:       # если нет, то  создаем  файл stat.json и
+            file.write("[]")                       # "создаем" в нем пустой список
+    with open('stat.json', 'r') as read_file:      # открываем файл stat.json
+        statistic_list = json.load(read_file)
+        statistic_list.append(information_dict)    # добавляем в список словарь с данными: {имя файла: {информация}}
     with open('stat.json', 'w') as write_file:
-        json.dump(stat_dict, write_file)
+        json.dump(statistic_list, write_file)      # записываем новые данные в stat.json
+
+
+def save_in_csv_file(information_dict: dict):
+    """
+    На вход функция получает словарь с информацией о фaйле.
+    В результате работы в файл stat.csv, дописывается информация о новом цензурируемом файле.
+    :param information_dict:
+    :return:
+    """
+
+    import csv
+    import os
+
+    if not os.path.isfile('stat.csv'):
+        with open('stat.csv', "w", encoding='cp1251', newline='') as header:
+            writer = csv.writer(header, delimiter=';')
+            writer.writerow(('filename', 'word', 'count', 'filename with changes'))
+
+    with open('stat.csv', "a", encoding='cp1251', newline='') as filename:
+        writer = csv.writer(filename, delimiter=';')
+        writer.writerow((f'{list(information_dict.keys())[0]}', '', '', f'{information_dict["filename_with_changes"]}'))
+
+    for key, value in information_dict[list(information_dict.keys())[0]].items():
+        with open('stat.csv', 'a', encoding='cp1251', newline='') as word_count:
+            writer = csv.writer(word_count, delimiter=';')
+            writer.writerow((' ', key, value, ' '))
--- Lesson_12_ basic.py
+++ Lesson_12_ basic.py
@@ -1,9 +1,11 @@
 from Akhtyrskyi_lesson_12 import *
+import os
 
-print('Задание №1')
 file = input('Введите имя файла для рецензирования: ')
+while not os.path.isfile(f'{file}.txt'):
+    file = input('Файла с таким иненем не найдено. Введите корректное имя файла: ')
 replacement_words = input('Введите слова через пробел, которые нужно заменить: ')
 filename_to_save = input('Введите имя файла, в который сохранить результат: ')
 cenzor_text(file, replacement_words, filename_to_save)
-print('Задание №2')
-statistics(file, replacement_words, count_words(file), filename_to_save)
+save_in_json_file(file_information(file, count_changed_words(file, replacement_words), filename_to_save))
+save_in_csv_file(file_information(file, count_changed_words(file, replacement_words), filename_to_save))
